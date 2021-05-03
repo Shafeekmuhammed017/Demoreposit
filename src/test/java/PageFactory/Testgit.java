7b3c022fc3efbade59e6cbaf3b7e03e59d923213
Shafeekmuhammed017 <83603382+Shafeekmuhammed017@users.noreply.github.com>
2021-05-04 01:09:36 +0530

    commited from reporsitory shafeek

--- src/test/java/PageFactory/Testgit.java
+++ src/test/java/PageFactory/Testgit.java
@@ -3,5 +3,6 @@ package PageFactory;
 public class Testgit {
     public static void main(String[] args) {
         System.out.println("Test git push");
+        System.out.println("Test git for pull testing added one more code");
     }
 }
